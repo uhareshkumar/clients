143d8d48e212fdd9de850125c1d1a82cfac86eeb
uhareshkumar <uharesh.kumar2015@vit.ac.in>
2020-07-28 18:35:04 +0530

    ui

--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -8,7 +8,17 @@ class Home extends React.Component {
                 <div className="sub-header">
                     Welcome to LowestEver
                 </div>
-                
+
+                <div className="sub-content">
+                    <i class="chevron circle left icon"></i>
+                    <div className="slider">
+                        <div className="card">hey</div>
+                        <div className="card">hey</div>
+                        <div className="card">hey</div>
+                        <div className="card">hey</div>
+                    </div>
+                    <i class="chevron circle right icon"></i>
+                </div>
             </div>
         );
     }
