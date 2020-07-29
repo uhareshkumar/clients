169b14050103150e617bc17c35cd423bdea09d3a
uhareshkumar <uharesh.kumar2015@vit.ac.in>
2020-07-29 19:15:48 +0530

    Updates

---
+++ precache-manifest.f63e5ac6bcb6063eaafe8be8bb0f7945.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ae2f0988d195144165c89d33bfe2829",
+    "url": "/clients/index.html"
+  },
+  {
+    "revision": "3977cd999d4b1b5a911e",
+    "url": "/clients/static/css/main.ebaa4cab.chunk.css"
+  },
+  {
+    "revision": "7271d685dc3cd8fc4e0e",
+    "url": "/clients/static/js/2.0123bedc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/clients/static/js/2.0123bedc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3977cd999d4b1b5a911e",
+    "url": "/clients/static/js/main.900410a3.chunk.js"
+  },
+  {
+    "revision": "d37e7d2693227788e63e",
+    "url": "/clients/static/js/runtime-main.50a6bb63.js"
+  },
+  {
+    "revision": "23a26676c7b78f3aa1c0c0e718400c2b",
+    "url": "/clients/static/media/logo.23a26676.png"
+  },
+  {
+    "revision": "ce63d9d470eaa832a1ecd01f5834927a",
+    "url": "/clients/static/media/one.ce63d9d4.jpg"
+  },
+  {
+    "revision": "eb410b7cb68a2c6d50847b1e076b40a0",
+    "url": "/clients/static/media/small-card.eb410b7c.jpg"
+  }
+]);
